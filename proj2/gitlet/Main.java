81fded3b9b5ec4144ddfc84c834d0e95d6a19785
ljy <123@123.com>
2024-08-24 17:50:37 +0800

    fix bug -checkout branchName

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -73,13 +73,12 @@ public class Main {
                     if (args.length == 2) {
                         Repository.CheckOut3(args[1]);
                     }
-                    else if(args[1].equals("--"))
-                    {
-                        Repository.CheckOut1(args[2]);
-                    }
-                    else if(args[2].equals("--"))
-                    {
-                        Repository.CheckOut2(args[1],args[3]);
+                    else {
+                        if (args[1].equals("--")) {
+                            Repository.CheckOut1(args[2]);
+                        } else if (args[2].equals("--")) {
+                            Repository.CheckOut2(args[1], args[3]);
+                        }
                     }
                     break;
 
